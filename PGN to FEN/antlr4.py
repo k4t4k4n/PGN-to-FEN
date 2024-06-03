65a877a162433e2527a434bd4be511a70ed48001
Jan Gurgul <115052674+k4t4k4n@users.noreply.github.com>
2024-06-03 02:41:23 +0200

    Delete PGN to FEN/antlr4.py

--- PGN to FEN/antlr4.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-from antlr4 import *
-
-def run_antlr_tool():
-    os.system('java -jar C:\\Users\\jangu\\Downloads\\antlr-4.13.1-complete.jar -Dlanguage=Python3 PGN.g4')
-
-if __name__ == "__main__":
-    run_antlr_tool()
