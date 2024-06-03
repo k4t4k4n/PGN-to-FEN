a9627b56f7a1479c88248d0a121700995ea04c84
Jan Gurgul <115052674+k4t4k4n@users.noreply.github.com>
2024-06-03 18:31:35 +0200

    Add files via upload

--- PGN to FEN/main.py
+++ PGN to FEN/main.py
@@ -5,8 +5,8 @@ from PGNParser import PGNParser
 from PGNListener import PGNListener
         
 
-def parse_pgn():
-    input_file = FileStream('test.pgn', encoding='utf-8')
+def parse_pgn(file_name):
+    input_file = FileStream(file_name, encoding='utf-8')
     lexer = PGNLexer(input_file)
     token_stream = CommonTokenStream(lexer)
     parser = PGNParser(token_stream)
@@ -19,5 +19,10 @@ def parse_pgn():
     return listener.get_moves()
 
 if __name__ == '__main__':
-    moves = parse_pgn()
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <filename>")
+        sys.exit(1)
+    
+    file_name = sys.argv[1]
+    moves = parse_pgn(file_name)
     #for move in moves: print(move)
